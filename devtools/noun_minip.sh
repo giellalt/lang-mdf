51cb2cdc5a1c6b7e0819a9c7d23bd79b6c1cffc8
Trond Trosterud <trond.trosterud@uit.no>
2018-02-05 17:25:47 +0000

    Adding miniparadigm generation routine.

---
+++ devtools/noun_minip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_contlex_para.sh PATTERN
+# example, when you are in smn:
+# sh devtools/noun_minip.sh LAAVU | less
+# sh devtools/noun_minip.sh smiergâs 
+# Only get the lemma you ask for:
+# sh devtools/noun_minip.sh '^smiergâs[:+]' 
+
+
+LOOKUP=$(echo $LOOKUP)
+GTHOME=$(echo $GTHOME)
+
+
+PATTERN=$1
+L_FILE="in.txt"
+cat src/morphology/stems/nouns.lexc src/morphology/stems/nouns_newwords.lexc | cut -d '!' -f1 | egrep $PATTERN | cut -d ':' -f1>$L_FILE
+
+P_FILE="test/data/testnounparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+   echo "${lemma}${form}" | $LOOKUP $GTHOME/langs/mdf/src/generator-gt-norm.xfst
+ done
+done
+
