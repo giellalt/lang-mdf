9490db15b7d9efafe70e14f22fbc09f4d4c0173b
Jack Rueter <rueter.jack@gmail.com>
2023-05-25 20:16:50 +0300

    Add urj-mdf-conversion.pl
    Still looking for how to get it to work.
    I was unable to draw any conclusions from lang-mns

---
+++ src/scripts/urj-mdf-conversion.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+#
+# urj-mdf-conversion.pl
+# Convert names in the URJ propernoun lexicon to mdf.
+
+use strict;
+use utf8;
+use feature 'unicode_strings';
+BEGIN {
+       $| = 1;
+       binmode(STDIN, ':encoding(UTF-8)');
+       binmode(STDOUT, ':encoding(UTF-8)');
+}
+use open qw( :encoding(UTF-8) :std );
+
+while(<>) {
+	# Continuation lexicon substitutions:
+#	s/ C-FI-NEN/nen LONDON/g ;
+	s/ PROP-PLC_KEL1/ N_KIAL1/g ;
+	s/ PROP-PLC_VELE/ N_TISHE/g ;
+	s/ PROP_VELE/ N_TISHE/g ;
+
+	
+# loanwords with compound border over identical vowels
+	s/Hjarteelva/Hjarte-elva/g ;
+	
+
+# names with Inari Saami inflection
+	s/^Aanaar\+/!Aanaar+/g ;
+
+# sme special symbols
+	s/b9/b/g ;
+
+	my $line = $_;
+
+	print $line;
+}
+
