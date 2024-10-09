5f39a02a59d45efa6242ccf63c612044db14497c
Jack Rueter <rueter.jack@gmail.com>
2024-10-09 15:17:13 +0300

    Retain PROP_ prefix in conversion of urj

--- src/fst/scripts/urj-mdf-conversion.pl
+++ src/fst/scripts/urj-mdf-conversion.pl
@@ -16,14 +16,15 @@ use open qw( :encoding(UTF-8) :std );
 while(<>) {
 	# Continuation lexicon substitutions:
 #	s/ C-FI-NEN/nen LONDON/g ;
-	s/ PROP_KAL_PLC/ N_KAL/g ;
-	s/ PROP_KUDO_PLC/ N_AVA/g ;
-	s/ PROP_OSH_PLC/ N_OSH/g ;
-	s/ PROP-PLC_KEL1/ N_KIAL1/g ;
-	s/ PROP-PLC_VELE/ N_TISHE/g ;
-	s/ PROP_VELE/ N_TISHE/g ;
-	s/ PROP-PLC_KUDO/ N_AVA/g ;
-	s/ PROP_KUDO/ N_AVA/g ;
+	s/ PROP_KAL_PLC/ PROP_KAL/g ;
+	s/ PROP_KUDO_PLC/ PROP_AVA/g ;
+	s/ PROP_OSH_PLC/ PROP_OSH/g ;
+	s/ PROP-PLC_KEL1/ PROP_KIAL1/g ;
+	s/ PROP-PLC_VELE/ PROP_TISHE/g ;
+	s/ PROP_VELE/ PROP_TISHE/g ;
+	s/ PROP-PLC_KUDO/ PROP_AVA/g ;
+	s/ PROP_KUDO/ PROP_AVA/g ;
+	s/ PROP_RUS_JA/ PROP_AVA/g ;
 
 	
 # loanwords with compound border over identical vowels
